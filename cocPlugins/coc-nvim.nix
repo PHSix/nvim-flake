ee2e208d59f67eaa84d8ead6060cad202fc1754e
ph <chenyi.ph@outlook.com>
2025-06-10 23:20:00 +0800

    coc.nvim update

--- cocPlugins/coc-nvim.nix
+++ cocPlugins/coc-nvim.nix
@@ -1,12 +1,17 @@
 { pkgs, fetchFromGitHub, ... }:
 pkgs.vimUtils.buildVimPlugin {
   pname = "coc.nvim";
-  version = "2025-06-02";
+  version = "2025-06-10";
   src = fetchFromGitHub {
     owner = "neoclide";
     repo = "coc.nvim";
-    rev = "9f7f280194f70229ec667721373a74d238618220";
-    hash = "sha256-w56LuUr30ubqvby6FppMlyhBZQwxvUhebSadpltgty4=";
+    rev = "aa96f1ddd567ac117fcc5cb24559f2d7a659ec85";
+    hash = "sha256-93xO8NXV1UHhznlVsnm53HeHsmAknSqLaUSkoCb3IwE=";
   };
+
+  nativeBuildInputs = with pkgs; [
+    nodejs
+  ];
+
   meta.homepage = "https://github.com/neoclide/coc.nvim";
 }
